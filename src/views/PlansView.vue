1c6665d926b5925587a897de1a55deaf7345298e
Jeabbabe <babe-ja@hotmail.com>
2023-02-11 16:10:59 +0700

    new commit

--- src/views/PlansView.vue
+++ src/views/PlansView.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>แผนการเรียน</h1>
+    <h2>ลงทะเบียนอยู่</h2>
   </div>
   <v-container>
     <v-row>
@@ -18,7 +19,14 @@
           </div>
 
           <v-card-actions>
-            <v-btn color="orange" variant="text"> สำรวจ </v-btn>
+            <v-btn
+              color="orange"
+              variant="text"
+              href="https://reg.mju.ac.th/registrar/class_info_2.asp?backto=home&option=0&courseid=8892990&acadyear=2565&semester=2&avs927644539=35"
+              target="_blank"
+            >
+              สำรวจ
+            </v-btn>
 
             <v-spacer></v-spacer>
 
@@ -33,11 +41,14 @@
               <v-divider></v-divider>
 
               <v-card-text>
-                I'm a thing. But, like most politicians, he promised more than he could
-                deliver. You won't have time for sleeping, soldier, not with all the bed
-                making you'll be doing. Then we'll go with that data file! Hey, you add a
-                one and two zeros to that or we walk! You're going to do his laundry? I've
-                got to find a way to escape.
+                <b>หน่วยกิต :</b> 3 (2-3-5) <br />
+                <b>คำอธิบายรายวิชา :</b> แนะนำสถาปัตยกรรมคอมพิวเตอร์
+                โครงสร้างและการทำงานของระบบคอมพิวเตอร์ หน่วยประมวลผลกลาง หน่วยความจำ
+                ระบบของการนําเข้าและการแสดงผล ระบบตัวเลขฐาน
+                การออกแบบหน่วยตรรกะและหน่วยควบคุม
+                หลักการทำงานและออกแบบด้านวงจรดิจิตอลและวงจรอิเล็กทรอนิกส์
+                การทำงานไมโครโพรเซสเซอร์เบื้องต้นสำหรับไอโอที วงจรรวม
+                ชุดของคำสั่งเครื่องการวัดสมรรถนะของคอมพิวเตอร์
               </v-card-text>
             </div>
           </v-expand-transition>
@@ -48,17 +59,24 @@
           ><v-card-title
             style="text-align: center; font-weight: bold; font-size: x-large"
           >
-            สถาปัตยกรรมคอมพิวเตอร์
+            การพัฒนาโปรแกรมประยุกต์<br />บนเว็บ
           </v-card-title>
           <v-card-subtitle style="text-align: center; font-size: larger"
-            >รหัส คพ232</v-card-subtitle
+            >รหัส คพ313</v-card-subtitle
           >
           <div class="text--primary" style="text-align: center">
-            ครูผู้สอน : ผศ.ภานุวัฒน์ เมฆะ
+            ครูผู้สอน : อ.อรรถวิท ชังคมานนท์
           </div>
 
           <v-card-actions>
-            <v-btn color="orange" variant="text"> สำรวจ </v-btn>
+            <v-btn
+              color="orange"
+              variant="text"
+              href="https://reg.mju.ac.th/registrar/class_info_2.asp?backto=home&option=0&courseid=8893593&acadyear=2565&semester=2&avs927650264=7"
+              target="_blank"
+            >
+              สำรวจ
+            </v-btn>
 
             <v-spacer></v-spacer>
 
@@ -73,11 +91,12 @@
               <v-divider></v-divider>
 
               <v-card-text>
-                I'm a thing. But, like most politicians, he promised more than he could
-                deliver. You won't have time for sleeping, soldier, not with all the bed
-                making you'll be doing. Then we'll go with that data file! Hey, you add a
-                one and two zeros to that or we walk! You're going to do his laundry? I've
-                got to find a way to escape.
+                <b>หน่วยกิต :</b> 3 (2-3-5)<br />
+                <b>คำอธิบายรายวิชา :</b> ความรู้เบื้องต้นทางเทคโนโลยี
+                เทคนิคการพัฒนาเว็บไซต์แบบไดนามิก
+                การออกแบบเว็บเพจที่ตอบสนองการแสดงผลให้เหมาะสมทั้งเบราว์เซอร์บนเว็บและอุปกรณ์เคลื่อนที่
+                การเขียนโปรแกรมบนฝั่งลูกข่ายและฝั่งแม่ข่ายเพื่อปฏิสัมพันธ์กับระบบฐานข้อมูล
+                การประยุกต์ใช้งานแอปพลิเคชันโปรแกรมมิ่งอินเตอร์เฟซ (เอพีไอ)
               </v-card-text>
             </div>
           </v-expand-transition>
@@ -88,17 +107,24 @@
           ><v-card-title
             style="text-align: center; font-weight: bold; font-size: x-large"
           >
-            สถาปัตยกรรมคอมพิวเตอร์
-          </v-card-title>
+            หลักสถิติ</v-card-title
+          >
           <v-card-subtitle style="text-align: center; font-size: larger"
-            >รหัส คพ232</v-card-subtitle
+            >สต301</v-card-subtitle
           >
           <div class="text--primary" style="text-align: center">
-            ครูผู้สอน : ผศ.ภานุวัฒน์ เมฆะ
+            ครูผู้สอน : อ.ดร.เชิดชัย มีเอียด
           </div>
 
           <v-card-actions>
-            <v-btn color="orange" variant="text"> สำรวจ </v-btn>
+            <v-btn
+              color="orange"
+              variant="text"
+              href="https://reg.mju.ac.th/registrar/class_info_2.asp?backto=home&option=0&courseid=63301&acadyear=2565&semester=2&avs927650264=38"
+              target="_blank"
+            >
+              สำรวจ
+            </v-btn>
 
             <v-spacer></v-spacer>
 
@@ -113,11 +139,12 @@
               <v-divider></v-divider>
 
               <v-card-text>
-                I'm a thing. But, like most politicians, he promised more than he could
-                deliver. You won't have time for sleeping, soldier, not with all the bed
-                making you'll be doing. Then we'll go with that data file! Hey, you add a
-                one and two zeros to that or we walk! You're going to do his laundry? I've
-                got to find a way to escape.
+                <b>หน่วยกิต :</b> 3 (3-0-6)<br />
+                <b>คำอธิบายรายวิชา :</b> ทบทวนแนวคิดเกี่ยวกับสถิติพรรณนาและความน่าจะเป็น
+                การแจกแจงความน่าจะเป็น การแจกแจงทวินาม การแจกแจงปกติ
+                การแจกแจงของกลุ่มตัวอย่าง
+                การประมาณค่าและการทดสอบสมมติฐานเกี่ยวกับค่าพารามิเตอร์
+                การวิเคราะห์ความแปรปรวน การวิเคราะห์การถดถอยและสหสัมพันธ์
               </v-card-text>
             </div>
           </v-expand-transition>
@@ -130,17 +157,24 @@
           ><v-card-title
             style="text-align: center; font-weight: bold; font-size: x-large"
           >
-            สถาปัตยกรรมคอมพิวเตอร์
+            ภาษาอังกฤษเชิงวิทยาศาสตร์<br />และเทคโนโลยี 2
           </v-card-title>
           <v-card-subtitle style="text-align: center; font-size: larger"
-            >รหัส คพ232</v-card-subtitle
+            >รหัส ศท242</v-card-subtitle
           >
           <div class="text--primary" style="text-align: center">
-            ครูผู้สอน : ผศ.ภานุวัฒน์ เมฆะ
+            ครูผู้สอน : อ.นิลุบล จิตต์มั่น
           </div>
 
           <v-card-actions>
-            <v-btn color="orange" variant="text"> สำรวจ </v-btn>
+            <v-btn
+              color="orange"
+              variant="text"
+              href="https://reg.mju.ac.th/registrar/class_info_2.asp?backto=home&option=0&courseid=8892870&acadyear=2565&semester=2&avs927652475=34"
+              target="_blank"
+            >
+              สำรวจ
+            </v-btn>
 
             <v-spacer></v-spacer>
 
@@ -155,11 +189,11 @@
               <v-divider></v-divider>
 
               <v-card-text>
-                I'm a thing. But, like most politicians, he promised more than he could
-                deliver. You won't have time for sleeping, soldier, not with all the bed
-                making you'll be doing. Then we'll go with that data file! Hey, you add a
-                one and two zeros to that or we walk! You're going to do his laundry? I've
-                got to find a way to escape.
+                <b>หน่วยกิต :</b> 3 (2-2-5)<br />
+                <b>คำอธิบายรายวิชา :</b>
+                คำศัพท์และโครงสร้างทางไวยากรณ์ภาษาอังกฤษในบริบททางด้านวิทยาศาสตร์และเทคโนโลยี
+                มุ่งเน้นการฝึกฝนทักษะภาษาอังกฤษและกลวิธีการเรียนภาษาอังกฤษ
+                เพื่อใช้สื่อสารและการเรียนในระดับที่สูงขึ้นทางด้านวิทยาศาสตร์และเทคโนโลยี
               </v-card-text>
             </div>
           </v-expand-transition>
@@ -170,17 +204,24 @@
           ><v-card-title
             style="text-align: center; font-weight: bold; font-size: x-large"
           >
-            สถาปัตยกรรมคอมพิวเตอร์
+            การวิเคราะห์และออกแบบ<br />เชิงวัตถุ
           </v-card-title>
           <v-card-subtitle style="text-align: center; font-size: larger"
-            >รหัส คพ232</v-card-subtitle
+            >รหัส คพ343</v-card-subtitle
           >
           <div class="text--primary" style="text-align: center">
-            ครูผู้สอน : ผศ.ภานุวัฒน์ เมฆะ
+            ครูผู้สอน : ผศ.ก่องกาญจน์ ดุลยไชย
           </div>
 
           <v-card-actions>
-            <v-btn color="orange" variant="text"> สำรวจ </v-btn>
+            <v-btn
+              color="orange"
+              variant="text"
+              href="https://reg.mju.ac.th/registrar/class_info_2.asp?backto=home&option=0&courseid=8893595&acadyear=2565&semester=2&avs927652475=61"
+              target="_blank"
+            >
+              สำรวจ
+            </v-btn>
 
             <v-spacer></v-spacer>
 
@@ -195,11 +236,12 @@
               <v-divider></v-divider>
 
               <v-card-text>
-                I'm a thing. But, like most politicians, he promised more than he could
-                deliver. You won't have time for sleeping, soldier, not with all the bed
-                making you'll be doing. Then we'll go with that data file! Hey, you add a
-                one and two zeros to that or we walk! You're going to do his laundry? I've
-                got to find a way to escape.
+                <b>หน่วยกิต :</b> 3 (2-3-5)<br />
+                <b>คำอธิบายรายวิชา :</b> วัฏจักรการพัฒนาระบบ
+                บทบาทและคุณสมบัติของนักวิเคราะห์ระบบ ความต้องการของระบบ คุณสมบัติของระบบ
+                พื้นฐานการวิเคราะห์และออกแบบระบบเชิงโครงสร้าง หลักการพื้นฐานเชิงวัตถุ
+                การวิเคราะห์เชิงวัตถุ แบบจำลองวัตถุ การออกแบบเชิงวัตถุ
+                การเชื่อมความสัมพันธ์ของวัตถุ การออกแบบส่วนติดต่อผู้ใช้
               </v-card-text>
             </div>
           </v-expand-transition>
@@ -210,17 +252,24 @@
           ><v-card-title
             style="text-align: center; font-weight: bold; font-size: x-large"
           >
-            สถาปัตยกรรมคอมพิวเตอร์
+            คณิตศาสตร์สำหรับวิทยาการ<br />คอมพิวเตอร์
           </v-card-title>
           <v-card-subtitle style="text-align: center; font-size: larger"
-            >รหัส คพ232</v-card-subtitle
+            >รหัส คพ252</v-card-subtitle
           >
           <div class="text--primary" style="text-align: center">
-            ครูผู้สอน : ผศ.ภานุวัฒน์ เมฆะ
+            ครูผู้สอน : อ.ดร.พยุงศักดิ์ เกษมสำราญ
           </div>
 
           <v-card-actions>
-            <v-btn color="orange" variant="text"> สำรวจ </v-btn>
+            <v-btn
+              color="orange"
+              variant="text"
+              href="https://reg.mju.ac.th/registrar/class_info_2.asp?backto=home&option=0&courseid=8892692&acadyear=2565&semester=2&avs927652475=71"
+              target="_blank"
+            >
+              สำรวจ
+            </v-btn>
 
             <v-spacer></v-spacer>
 
@@ -235,11 +284,122 @@
               <v-divider></v-divider>
 
               <v-card-text>
-                I'm a thing. But, like most politicians, he promised more than he could
-                deliver. You won't have time for sleeping, soldier, not with all the bed
-                making you'll be doing. Then we'll go with that data file! Hey, you add a
-                one and two zeros to that or we walk! You're going to do his laundry? I've
-                got to find a way to escape.
+                <b>หน่วยกิต :</b> 3 (3-0-6)<br />
+                <b>คำอธิบายรายวิชา :</b> ความรู้เบื้องต้นเกี่ยวกับเมตริกซ์
+                การดำเนินการของเมตริกซ์ การดำเนินการตามแถวและหลัก
+                รูปแบบแอชชิลอนของเมตริกซ์ ระบบสมาการเชิงเส้น ดีเทอร์มิแนนท์
+                ปริภูมิเวกเตอร์ การโปรแกรมเชิงเส้น
+              </v-card-text>
+            </div>
+          </v-expand-transition>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+  <div>
+    <h2>เปิดให้ลงทะเบียนเพิ่ม</h2>
+  </div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-card class="mx-auto" width="344">
+          <v-card-title style="text-align: center; font-weight: bold; font-size: x-large">
+            ฐานข้อมูลแบบไม่มีโครงสร้าง
+          </v-card-title>
+          <v-card-subtitle style="text-align: center; font-size: larger"
+            >รหัส 10301224
+          </v-card-subtitle>
+          <v-spacer></v-spacer>
+          <div class="text--primary" style="text-align: center">ครูผู้สอน :</div>
+
+          <v-card-actions>
+            <v-btn color="orange" variant="text" href="" target="_blank"> สำรวจ </v-btn>
+
+            <v-spacer></v-spacer>
+
+            <v-btn
+              :icon="show1 ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+              @click="show1 = !show1"
+            ></v-btn>
+          </v-card-actions>
+
+          <v-expand-transition>
+            <div v-show="show1">
+              <v-divider></v-divider>
+
+              <v-card-text>
+                <b>หน่วยกิต :</b> 3 (2-3-5) <br />
+                <b>คำอธิบายรายวิชา :</b>
+              </v-card-text>
+            </div>
+          </v-expand-transition>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card class="mx-auto" width="344"
+          ><v-card-title
+            style="text-align: center; font-weight: bold; font-size: x-large"
+          >
+            การพัฒนาระบบฝั่งเซิร์ฟเวอร์
+          </v-card-title>
+          <v-card-subtitle style="text-align: center; font-size: larger"
+            >รหัส 10301232</v-card-subtitle
+          >
+          <div class="text--primary" style="text-align: center">ครูผู้สอน :</div>
+
+          <v-card-actions>
+            <v-btn color="orange" variant="text" href="" target="_blank"> สำรวจ </v-btn>
+
+            <v-spacer></v-spacer>
+
+            <v-btn
+              :icon="show2 ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+              @click="show2 = !show2"
+            ></v-btn>
+          </v-card-actions>
+
+          <v-expand-transition>
+            <div v-show="show2">
+              <v-divider></v-divider>
+
+              <v-card-text>
+                <b>หน่วยกิต :</b> 3 (2-3-5)<br />
+                <b>คำอธิบายรายวิชา :</b>
+              </v-card-text>
+            </div>
+          </v-expand-transition>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card class="mx-auto" width="344"
+          ><v-card-title
+            style="text-align: center; font-weight: bold; font-size: x-large"
+          >
+            มนุษย์ สังคม เทคโนโลยีและสิ่งแวดล้อม</v-card-title
+          >
+          <v-card-subtitle style="text-align: center; font-size: larger"
+            >10700105</v-card-subtitle
+          >
+          <div class="text--primary" style="text-align: center">ครูผู้สอน :</div>
+
+          <v-card-actions>
+            <v-btn color="orange" variant="text" href="" target="_blank"> สำรวจ </v-btn>
+
+            <v-spacer></v-spacer>
+
+            <v-btn
+              :icon="show3 ? 'mdi-chevron-up' : 'mdi-chevron-down'"
+              @click="show3 = !show3"
+            ></v-btn>
+          </v-card-actions>
+
+          <v-expand-transition>
+            <div v-show="show3">
+              <v-divider></v-divider>
+
+              <v-card-text>
+                <b>หน่วยกิต :</b> 3 (3-0-6)<br />
+                <b>คำอธิบายรายวิชา :</b>
               </v-card-text>
             </div>
           </v-expand-transition>
